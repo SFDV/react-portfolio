03023e9b38940a111b4b59496a9fed39219642e4
SuperFamousDanV <djudev777@live.com>
2019-02-08 16:45:43 -0500

    Updates

---
+++ precache-manifest.08a3f95e239ac61608f23b6a655bf1df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "02720c430c91b8b1454a",
+    "url": "/react-portfolio/static/js/main.02720c43.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/react-portfolio/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "02720c430c91b8b1454a",
+    "url": "/react-portfolio/static/css/main.7087be52.chunk.css"
+  },
+  {
+    "revision": "1bd5cdb0320689276e27ef01da089911",
+    "url": "/react-portfolio/index.html"
+  }
+];
