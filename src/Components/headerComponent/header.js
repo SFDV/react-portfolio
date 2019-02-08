90b52039c7d7e6515abd558e1ee24b2b71688b4e
SuperFamousDanV <djudev777@live.com>
2019-02-08 17:36:42 -0500

    wrapped links in nav

--- src/Components/headerComponent/header.js
+++ src/Components/headerComponent/header.js
@@ -7,17 +7,19 @@ class Header extends Component {
       <header>
         LOGO
 
-        <ul>
-            <li>
-                <a href="#">Home</a>
-            </li>
-            <li>
-                <a href="#">Projects</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
-        </ul>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Projects</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
       </header>
     );
   }
