e09c678810c294a9586d5f4d933ac324b3a52878
SuperFamousDanV <djudev777@live.com>
2019-02-11 12:54:04 -0500

    Incorporation Bootstrap

--- src/Components/headerComponent/header.js
+++ src/Components/headerComponent/header.js
@@ -5,21 +5,24 @@ class Header extends Component {
   render() {
     return (
       <header>
-        LOGO
 
-        <nav>
-            <ul>
-                <li>
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#">Projects</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-        </nav>
+        <div class="container">
+
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
       </header>
     );
   }
