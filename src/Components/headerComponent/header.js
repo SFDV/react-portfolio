682d28f4a12e76f205498029310f3685b2a57af2
SuperFamousDanV <djudev777@live.com>
2019-02-19 20:57:40 -0500

    added hover properties to navbar

--- src/Components/headerComponent/header.js
+++ src/Components/headerComponent/header.js
@@ -8,23 +8,19 @@ class Header extends Component {
 
         <div class="container">
 
-            <p class="logo">
-                Dan Vickey
-            </p>
-
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">Projects</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </nav>
+          <nav>
+              <ul>
+                  <li>
+                      <a class="home" href="#">Home</a>
+                  </li>
+                  <li>
+                      <a class="projects" href="#">Projects</a>
+                  </li>
+                  <li>
+                      <a class="contact" href="#">Contact</a>
+                  </li>
+              </ul>
+          </nav>
 
         </div>
       </header>
