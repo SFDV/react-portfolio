789c36a746eda765666bd6c4278c0934bcced9bf
SuperFamousDanV <djudev777@live.com>
2019-02-21 17:39:47 -0500

    Created footer

--- src/Components/footerComponent/footer.js
+++ src/Components/footerComponent/footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './footer.css';
 
 class Footer extends Component {
   render() {
     return (
         <footer>
-            Footer
+            <div class="container">
+              <p>© Dan Vickey 2019</p>
+            </div>
         </footer>
     );
   }
